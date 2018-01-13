be7d5a4b08f841d516b226d28d8b359f0404f085
cjett-embyr <33522960+cjett-embyr@users.noreply.github.com>
2018-01-12 19:33:02 -0600

    Updated code for more modular use

--- ledlightsWifi.ino
+++ ledlightsWifi.ino
@@ -1,22 +1,22 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
  
-const char* ssid = "Bees?";
-const char* password = "BBBBBBBBBB";
+const char* ssid = "";//put in the ssid before flash
+const char* password = "";//put in the password before flash
 WiFiUDP udpClient;
-char packetBuffer[255]; //buffer to hold incoming packet
+char packetBuffer[256]; //buffer to hold incoming packet (256 characters)
 
 WiFiServer server(80);
  
 void setup() {
   Serial.begin(115200);
-  Serial.println("1282550005000");
+  //Serial.println("1282550005000");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
   udpClient.begin(1337);
-  Serial.println("00000000010000");
+  //Serial.println("00000000010000");
 }
  
 void loop() {
